2628d7ec7f7e1473e63d96830d591bb16544086f
Igor <igor35hh@gmail.com>
2017-04-25 11:51:25 +0300

    prefinished

--- ToMySQL/src/ToMySQL.cpp
+++ ToMySQL/src/ToMySQL.cpp
@@ -7,9 +7,91 @@
 //============================================================================
 
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+
+#include <cstdlib>
+#include <mysql_com.h>
+
+#include "BankAccount.h"
+#include "BankTransaction.h"
+
+enum Options{ PRINT = 1, NEW, WITHDRAW, DEPOSIT, CLOSE, END };
+
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+int mainMenu() {
+
+	cout << "\nMenu Options" << endl << "1 Print All Account"
+		 << endl << "2 - Open new Account" << endl << "3 - Withdraw" << endl << "4 - Deposit"
+		 << endl << "5 - Close Account" << endl << "6 - End Transaction" << endl;
+
+	int ch;
+	cin >> ch;
+	return ch;
+
+}
+
+int main(int argc, char** argv) {
+
+	BankTransaction* bt = new BankTransaction("localhost", "root", "mysql", "mybank");
+
+	int choice;
+	int acno;
+	string fname, lname;
+	double bal;
+
+	while(1) {
+		choice = mainMenu();
+		if(choice == END) {
+			break;
+		}
+		switch(choice) {
+		case PRINT:
+			bt->printAllAccounts();
+			break;
+		case NEW:
+			cout << "\nEnter account no, first name, last name, balance: " << endl << "? ";
+			cin >> acno;
+			cin >> fname;
+			cin >> lname;
+			cin >> bal;
+			if(acno < 1) {
+				cout << "Invalid account number" << endl;
+				break;
+			}
+			bt->createAccount(new BankAccount(acno, fname, lname, bal));
+			break;
+		case WITHDRAW:
+			cout << "\nEnter account no, amount to withdraw" << endl << "? ";
+			cin << acno;
+			cin << bal;
+			if(bal < 0) {
+				cout << "Invalid amount" << endl;
+				break;
+			}
+			bt->withdraw(acno, bal);
+			break;
+		case DEPOSIT:
+			cout << "\nEnter eccount no, amount to deposit" << endl << "? ";
+			cin >> acno;
+			cin >> bal;
+			if(bal < 0) {
+				cout << "Invalid amount" << endl;
+				break;
+			}
+			bt->deposit(acno, bal);
+			break;
+		case CLOSE:
+			cout << "\nEnter account no to close account" << endl << "? ";
+			cin >> acno;
+			bt->closeAccount(acno);
+			break;
+		default:
+			cerr << "Invalid choice" << endl;
+			break;
+		}
+	}
+
 	return 0;
 }
